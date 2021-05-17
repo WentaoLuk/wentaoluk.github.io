f2bc16dab81e2471e6d19fabf8e0e0e0bedb5398
Wentao Lu <wentaoluk@outlook.com>
2021-05-17 04:00:17 -0400

    Updates

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-0f2d12fa22e0572cb67a.js"
+    "url": "webpack-runtime-dd5a9886973f60caa628.js"
   },
   {
     "url": "framework-741ade27086b2708e961.js"
@@ -37,7 +37,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "fc9d059ea98ba7b9008a338cbb005f77"
+    "revision": "913a628db9bfde927247312f326de594"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-ad6431e4664bcf916d19.js"
@@ -48,7 +48,7 @@ self.__precacheManifest = [
   },
   {
     "url": "page-data/app-data.json",
-    "revision": "57b28c0a622b7c0a086bad14c912c6ab"
+    "revision": "831a3b09ff191f7c37f5f561e589fd21"
   },
   {
     "url": "polyfill-4c9e5ca557f297a65cac.js"
